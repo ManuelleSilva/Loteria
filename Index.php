667b47e5744aa01a4baa464f5ce961e2c2d76915
Manuelle Martins da Silva <141978194+ManuelleSilva@users.noreply.github.com>
2024-03-27 22:41:31 -0300

    Update Index.php (estilização-fase inicial)

--- Index.php
+++ Index.php
@@ -2,77 +2,21 @@
 <html>
 <head>
     <title>LOTO GRU</title>
-    <style>
-        body {
-            text-align: center;
-            margin-top: 50px;
-        }
-
-        .checkbox-container {
-            display: inline-block;
-            margin: 10px;
-            cursor: pointer;
-            padding: 17px 17px;
-            font-size: 15px;
-            border: 1px solid #ccc;
-            background-color: #fff;
-            border-radius: 2px;
-            position: relative; 
-            user-select: none;
-        }
-
-        .checkbox-container input {
-            position: absolute;
-            opacity: 0;
-            cursor: pointer;
-            top: 0;
-            left: 0;
-            margin: 0;
-            padding: 0;
-            z-index: 1; 
-        }
-
-        .checkmark {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%); 
-            background-color: #eee;
-            border-radius: 3px;
-            width: 20px;
-            height: 20px;
-            line-height: 20px;
-            text-align: center;
-            z-index: 0;
-        }
-
-        .checkbox-container:hover .checkmark {
-            background-color: #ccc;
-        }
-
-        .checkbox-container input:checked + .checkmark {
-            background-color: #2196F3;
-        }
-
-        .checkmark:after {
-            content: attr(data-number);
-            z-index: 1;
-        }
-    </style>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form method="post" action="procLoteria.php">
-
-        <label for="valor_aposta">Valor da Aposta:</label>
-        <input type="number" id="valor_aposta" name="valor_aposta" required><br>
-
+    <form method="post" action="loto.php">
+        <div>
+        <label for="valor_aposta" class="lb_valor_aposta">Valor da Aposta:</label>
+        <input type="number" id="valor_aposta" class="valor_aposta" name="valor_aposta" required><br>
+</div><div>
         <label for="numeros">Escolha 25 números entre 1 e 50:</label><br>
         <?php
         for ($i = 1; $i <= 50; $i++) {
             echo "<label class='checkbox-container'><input type='checkbox' name='numeros[]' value='$i'><span class='checkmark' data-number='$i'></span></label>";
         }
         ?>
-
+</div><div>
         <br><strong><span id="numSelecionados">0</span> números selecionados<br></strong>
 
         <script>
@@ -83,10 +27,10 @@
                 });
             });
         </script>
-
+</div><div>
         <br>
         <input type="submit" value="Apostar">
-
+        </div>
     </form>
 </body>
 </html>
